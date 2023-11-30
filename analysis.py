322cbd2640b7474fb4f47057d933dda9d68a1003
O.J. Parker <78820644+codenodejode74@users.noreply.github.com>
2023-11-30 04:05:09 -0500

    pog update
    + shows weather station id and protocol when hovering.
    + now a scatterplot

--- analysis.py
+++ analysis.py
@@ -3,25 +3,49 @@
 import pandas as pd
 import plotly.graph_objects as pl
 from plotly.subplots import make_subplots
+import argparse
 
-def read_csv(file):
+# Create the parser
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+# Add the arguments
+parser.add_argument('csv_file', type=str, help='The CSV file to process')
+parser.add_argument('--id_pws', type=str, help='The id_pws to focus on. If not provided, all id_pws will be used.')
+
+# Parse the arguments
+args = parser.parse_args()
+
+print(f'CSV file: {args.csv_file}') # TESTING 
+print(f'id_pws: {args.id_pws}') # TESTING
+
+
+def read_csv(file, id_pws=None):
+    print(f'Reading CSV file: {file}') # TESTING
     column_names = ['temperature_fz', 'humidity_fz', 'pressure_fz', 'lat', 'long', 'time_gps', 'numSats_gps', 'temperature_pws', 'humidity_pws', 'time_pws', 'protocol_pws', 'id_pws']
     df = pd.read_csv(file, names=column_names, usecols=range(12))
+    print(f'id_pws column: {df["id_pws"]}') # TESTING
+    print(f'Unique id_pws in the DataFrame: {df["id_pws"].unique()}') # TESTING
+    if id_pws:
+        print(f'Focusing on id_pws: {id_pws}') # TESTING
+        df = df[df['id_pws'] == id_pws]
+        print(f'Unique id_pws in the DataFrame after filtering: {df["id_pws"].unique()}') # TESTING
     return df
 
 def plot_with_plotly(df):
+    print('Plotting data...') # TESTING
+
     # Create subplots
     fig = make_subplots(rows=3, cols=1, shared_xaxes=True, subplot_titles=("Temperature", "Humidity", "Pressure"))
 
     # sub plot lines
-    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['temperature_fz'], mode='lines', name='Temperature FZ'), row=1, col=1)
-    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['temperature_pws'], mode='lines', name='Temperature PWS'), row=1, col=1)
+    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['temperature_fz'], mode='markers', name='Temperature FZ', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=1, col=1)
+    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['temperature_pws'], mode='markers', name='Temperature PWS', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=1, col=1)
 
-    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['humidity_fz'], mode='lines', name='Humidity FZ'), row=2, col=1)
-    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['humidity_pws'], mode='lines', name='Humidity PWS'), row=2, col=1)
+    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['humidity_fz'], mode='markers', name='Humidity FZ', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=2, col=1)
+    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['humidity_pws'], mode='markers', name='Humidity PWS', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=2, col=1)
 
-    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['pressure_fz'], mode='lines', name='Pressure FZ'), row=3, col=1)
-    # fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['pressure_pws'], mode='lines', name='Pressure PWS'), row=3, col=1) # Currently no pressure from PWS
+    fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['pressure_fz'], mode='markers', name='Pressure FZ', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=3, col=1)
+    # fig.add_trace(pl.Scatter(x=df['time_gps'], y=df['pressure_pws'], mode='markers', name='Pressure PWS', text=df.apply(lambda row: f'protocol_pws: {row.protocol_pws}, id_pws: {row.id_pws}', axis=1)), row=3, col=1) # Currently no pressure from PWS
 
     # title at top
     fig.update_layout(title='Meteorological Data vs. Time')
@@ -36,5 +60,6 @@ def plot_with_plotly(df):
     # Show plot
     fig.show()
 
-df = read_csv('./sample_data.csv')
+# use args.csv_file as the CSV file name
+df = read_csv(args.csv_file, args.id_pws)
 plot_with_plotly(df)
